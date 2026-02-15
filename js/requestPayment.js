24e571896508eb38cd09bc1229d33883c5e37822
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-02-15 10:21:31 +0530

    Update requestPayment.js

--- js/requestPayment.js
+++ js/requestPayment.js
@@ -1,2 +1,2 @@
-const API="https://supply-chain-backend-production-36ed.up.railway.app";
+const API="https://c-production-df72.up.railway.app";
 function sendRequest(role){fetch(API+"/payment-request",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({role,amount:amount.value,reason:reason.value})});alert("Request sent");}
